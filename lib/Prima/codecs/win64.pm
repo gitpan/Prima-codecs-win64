c1ccf1371e843eddf233d82b346502c28a9df26f
Dmitry Karasik <dmitry@karasik.eu.org>
2012-02-07 19:35:40 +0000

    Import of KARASIK/Prima-codecs-win64-1.02 from CPAN.
    gitpan-cpan-distribution: Prima-codecs-win64
    gitpan-cpan-version:      1.02
    gitpan-cpan-path:         KARASIK/Prima-codecs-win64-1.02.tar.gz
    gitpan-cpan-author:       KARASIK
    gitpan-cpan-maturity:     released

--- lib/Prima/codecs/win64.pm
+++ lib/Prima/codecs/win64.pm
@@ -1,7 +1,7 @@
 package Prima::codecs::win64;
 use vars qw($VERSION);
 
-$VERSION = '1.01';
+$VERSION = '1.02';
 
 1;
 
@@ -11,13 +11,13 @@ __DATA__
 
 =head1 NAME 
 
-Prima::codec::win64 - binary distribution of image libraries for win64
+Prima::codecs::win64 - binary distribution of image libraries for win64
 
 =head1 DISTRIBUTION
 
 L<Prima> needs image libraries to work correctly. Unix builds rely
 on existing installations of C<libjpeg>, C<libgif/libungif>, C<libtiff>, C<libpng>,
-C<libXpm>, and C<libX11>, all of them being optional dependencies.
+and C<libXpm>, all of them being optional dependencies.
 
 This binary distribution provides the libraries above and their include files,
 for MSVC build.
